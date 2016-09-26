8d8b82022c447d8a5ce35f556777c276b25c5f97
Dmitry Panchenko <cancerogenic@gmail.com>
2016-09-26 04:08:20 +0300

    added basic check

--- battleship_panchenko.py
+++ battleship_panchenko.py
@@ -25,6 +25,22 @@ class Cell(object):
             return ' ? |'
 
 
+class Ship(Cell):
+    def __init__(self, user, parts):
+        self.user = user
+        self.sunk = False
+        self.parts = None
+
+    def check_ship_sunk(self):
+        return None
+
+    @classmethod
+    def create_ship(cls):
+        user = 'Player1'
+        sunk = False
+        parts = get_ship_coords()
+
+
 def render_board():
     for row in board:
         print('|', end='')
@@ -33,19 +49,26 @@ def render_board():
         print()
 
 
-def get_ship_coords(a=None):
-    if a is None:
-        print('Player1, input ship coords, axis (h/v), size (2,2,h,3): ', end='')
-        pre_coords = input()
-    else:
-        pre_coords = a
+def get_ship_coords():
+    print('Player1, input ship coords (horizontal then vertical), axis (h/v), size (2,2,h,3): ', end='')
 
-    coords = pre_coords.split(',')
-    x, y, axis, size = tuple(coords)
-    x = int(x)
-    y = int(y)
-    axis = axis.strip()
-    size = int(size)
+    while True:
+        pre_coords = input()
+        coords = pre_coords.split(',')
+        x, y, axis, size = tuple(coords)
+        x = int(y) - 1
+        y = int(y) - 1
+        axis = axis.strip()
+        size = int(size)
+        if x > 10 or y > 10 or x < 0 or y < 0:
+            print('Wrong coordinates! Try again!')
+        elif axis == 'h' and (x + size) > 10:
+            print('Ship out of board! Try again!')
+        elif axis == 'v' and (y + size) > 10:
+            print('Ship out of board! Try again!')
+        else:
+            print('Ship added!')
+            break
 
     res = []
 
@@ -75,7 +98,7 @@ def main():
             live_cell = Cell(i, j)
             board_row.append(live_cell)
 
-    res = get_ship_coords('3,4,h,3')
+    res = get_ship_coords()
     print(res)
 
     for pair in res:
@@ -83,7 +106,6 @@ def main():
         c = board[y][x]
         c.is_ship = True
 
-    render_board()
     fire(4,4)
     for x in range(0,10):
         for y in range(0,10):
